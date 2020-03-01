433dee3e412d4385c9fe593edee49e77aaea7816
gness1804 <graham.nessler@gmail.com>
2020-03-01 17:45:37 -0600

    First pass: got first four tests passing

--- rna-transcription.ts
+++ rna-transcription.ts
@@ -1,7 +1,16 @@
 class Transcriptor {
-    toRna( /* Parameters go here */ ) {
-        // Your code here
-    }
+  static sequenceMap = new Map([
+    ["C", "G"],
+    ["G", "C"],
+    ["T", "A"],
+    ["A", "U"]
+  ]);
+
+  toRna(nucleotide: string): string {
+    if (!Transcriptor.sequenceMap.has(nucleotide))
+      throw new Error("Invalid input DNA.");
+    return Transcriptor.sequenceMap.get(nucleotide) || "";
+  }
 }
 
-export default Transcriptor
+export default Transcriptor;
